8af5b04f8218d60bd295fcc6de865a26a23029ac
Sukhmapreet Singh <sukhmansekhon789@gmail.com>
2025-09-14 05:55:07 +0530

    Deleted previous code and writing new one for more efficiency.

--- input.py
+++ input.py
@@ -1,6 +1,1 @@
-# re: Python’s regular expression module. We use it to extract words from text.
-#Counter: A dictionary subclass from collections that helps count occurrences of words efficiently.
-import re
-from collections import Counter
-
 
